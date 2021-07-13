d3520bd888488c4b95890e9926483faf4b4a0c6b
Henry Adarme <hsadarmem@unadvirtual.edu.co>
2021-07-13 10:37:11 -0500

    Agregar archivos de proyecto.

---
+++ Tema3Ejercicio15/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBoxNumbers = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LabelLarge = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TextBoxNumbers)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(776, 140)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Listado de números"
+        '
+        'TextBoxNumbers
+        '
+        Me.TextBoxNumbers.Location = New System.Drawing.Point(6, 79)
+        Me.TextBoxNumbers.Name = "TextBoxNumbers"
+        Me.TextBoxNumbers.Size = New System.Drawing.Size(764, 22)
+        Me.TextBoxNumbers.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(270, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Escriba los números separados por coma"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(333, 189)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(134, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "El número mayor es"
+        '
+        'LabelLarge
+        '
+        Me.LabelLarge.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelLarge.Location = New System.Drawing.Point(18, 228)
+        Me.LabelLarge.Name = "LabelLarge"
+        Me.LabelLarge.Size = New System.Drawing.Size(764, 34)
+        Me.LabelLarge.TabIndex = 2
+        Me.LabelLarge.Text = "0"
+        Me.LabelLarge.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 323)
+        Me.Controls.Add(Me.LabelLarge)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Encuentra el mayor"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBoxNumbers As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LabelLarge As Label
+End Class
---
+++ Tema3Ejercicio15/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles LabelLarge.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBoxNumbers.TextChanged
+        Dim vectoraux() As String
+        vectoraux = TextBoxNumbers.Text.Split({","}, StringSplitOptions.None)
+        Dim large As Integer = 0
+
+        For i As Integer = 0 To vectoraux.Count() - 1 Step 1
+            Dim Number As Integer = 0
+
+            If Integer.TryParse(vectoraux(i), Number) Then
+                If large < Number Then
+                    large = vectoraux(i)
+                End If
+            End If
+
+        Next
+
+        LabelLarge.Text = large
+
+    End Sub
+End Class
